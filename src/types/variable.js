6cd4c3f6b184b05fb7f9c42063c8571a2fac06b0
서재원 <sbshw1@gmail.com>
2018-07-09 00:15:46 +0900

    finish variable

---
+++ src/types/variable.js
@@ -0,0 +1,27 @@
+const { Type } = require('./type');
+
+class Variable {
+  constructor(type, value = type.defaultValue()) {
+    if(!Type.isType(type, Type.inferenceType(value)))
+      throw new TypeError('unexpected value');
+    this.type = type;
+    this.value = value;
+  }
+
+  setValue(value) {
+    if(!Type.isType(this.type, Type.inferenceType(value)))
+      throw new TypeError('unexpected value');
+    this.value = value;
+    return value;
+  }
+
+  bind(method) {
+    return () => this.type[method](this.value);
+  }
+
+  unwrap() {
+    return this.value;
+  }
+}
+
+module.exports = Variable;
